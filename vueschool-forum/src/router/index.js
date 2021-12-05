c645bb1700ab2921d5494f1e22a4e728bc8149b3
Airscript <francesco@airscript.it>
2021-12-05 19:15:14 +0000

    feat: add PageForum component

--- vueschool-forum/src/router/index.js
+++ vueschool-forum/src/router/index.js
@@ -1,28 +1,34 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import PageHome from '@/pages/PageHome'
-import PageThreadShow from '@/pages/PageThreadShow'
-import PageNotFound from '@/pages/PageNotFound'
-
+import Home from '@/pages/PageHome'
+import ThreadShow from '@/pages/PageThreadShow'
+import Forum from '@/pages/PageForum'
+import NotFound from '@/pages/PageNotFound'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'PageHome',
-      component: PageHome
+      name: 'Home',
+      component: Home
     },
     {
-      path: '/threads/:id',
-      name: 'PageThreadShow',
-      component: PageThreadShow,
+      path: '/forum/:id',
+      name: 'Forum',
+      component: Forum,
+      props: true
+    },
+    {
+      path: '/thread/:id',
+      name: 'ThreadShow',
+      component: ThreadShow,
       props: true
     },
     {
       path: '*',
       name: 'NotFound',
-      component: PageNotFound
+      component: NotFound
     }
   ],
   mode: 'history'
