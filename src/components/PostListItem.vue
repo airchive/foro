8986d5932358288c71ec5d86e62d31310039ebc5
Airscript <francesco@airscript.it>
2021-12-12 20:36:17 +0000

    refactor: implement higher order functions in posts related components

--- src/components/PostListItem.vue
+++ src/components/PostListItem.vue
@@ -7,6 +7,7 @@
         <img class="avatar-large" :src="user.avatar" alt="">
       </a>
 
+      <p class="desktop-only text-small">{{userThreadsCount}} threads</p>
       <p class="desktop-only text-small">{{userPostsCount}} posts</p>
     </div>
 
@@ -62,7 +63,11 @@
         },
 
         userPostsCount () {
-          return countObjectProperties(this.user.posts)
+          return this.$store.getters.userPostsCount(this.post.userId)
+        },
+
+        userThreadsCount () {
+          return this.$store.getters.userThreadsCount(this.post.userId)
         }
       }
     }
