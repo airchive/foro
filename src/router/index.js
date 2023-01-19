c23ac0aa2de11c565b42907f5af2501bdcf6e6fb
Airscript <francesco@airscript.it>
2023-01-19 20:01:59 +0100

    refactor: clean up index.js in router folder

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,16 @@ import Vue from 'vue'
 import store from '@/store'
 import Router from 'vue-router'
 import Home from '@/pages/PageHome'
-import ThreadShow from '@/pages/PageThreadShow'
-import ThreadCreate from '@/pages/PageThreadCreate'
-import ThreadEdit from '@/pages/PageThreadEdit'
-import Category from '@/pages/PageCategory'
 import Forum from '@/pages/PageForum'
-import Profile from '@/pages/PageProfile'
-import Register from '@/pages/PageRegister'
 import SignIn from '@/pages/PageSignIn'
+import Profile from '@/pages/PageProfile'
+import Category from '@/pages/PageCategory'
+import Register from '@/pages/PageRegister'
 import NotFound from '@/pages/PageNotFound'
+import ThreadShow from '@/pages/PageThreadShow'
+import ThreadEdit from '@/pages/PageThreadEdit'
+import ThreadCreate from '@/pages/PageThreadCreate'
+
 Vue.use(Router)
 
 const router = new Router({
@@ -18,81 +19,94 @@ const router = new Router({
     {
       path: '/',
       name: 'Home',
-      component: Home
+      component: Home,
     },
+
     {
-      path: '/category/:id',
+      props: true,
       name: 'Category',
       component: Category,
-      props: true
+      path: '/category/:id',
     },
+
     {
-      path: '/forum/:id',
+      props: true,
       name: 'Forum',
       component: Forum,
-      props: true
+      path: '/forum/:id',
     },
+
     {
-      path: '/thread/create/:forumId',
+      props: true,
       name: 'ThreadCreate',
       component: ThreadCreate,
+      meta: { requiresAuth: true },
+      path: '/thread/create/:forumId',
+    },
+
+    {
       props: true,
-      meta: { requiresAuth: true }
-    },
-    {
-      path: '/thread/:id',
       name: 'ThreadShow',
+      path: '/thread/:id',
       component: ThreadShow,
-      props: true
     },
+
     {
-      path: '/thread/:id/edit',
+      props: true,
       name: 'ThreadEdit',
       component: ThreadEdit,
-      props: true,
-      meta: { requiresAuth: true }
+      path: '/thread/:id/edit',
+      meta: { requiresAuth: true },
     },
+
     {
       path: '/me',
+      props: true,
       name: 'Profile',
       component: Profile,
-      props: true,
-      meta: { requiresAuth: true }
+      meta: { requiresAuth: true },
     },
+
     {
       path: '/me/edit',
-      name: 'ProfileEdit',
       component: Profile,
+      name: 'ProfileEdit',
       props: {edit: true},
-      meta: { requiresAuth: true }
+      meta: { requiresAuth: true },
     },
+
     {
-      path: '/register',
       name: 'Register',
+      path: '/register',
       component: Register,
-      meta: { requiresGuest: true }
+      meta: { requiresGuest: true },
     },
+
     {
-      path: '/signin',
       name: 'SignIn',
+      path: '/signin',
       component: SignIn,
-      meta: { requiresGuest: true }
+      meta: { requiresGuest: true },
     },
+
     {
       path: '/logout',
       name: 'SignOut',
       meta: { requiresAuth: true },
+
       beforeEnter (to, from, next) {
         store.dispatch('signOut')
           .then(() => next({name: 'Home'}))
-      }
+      },
     },
+
     {
       path: '*',
       name: 'NotFound',
-      component: NotFound
+      component: NotFound,
     }
   ],
+
   mode: 'history'
 })
 
@@ -102,14 +116,12 @@ router.beforeEach((to, from, next) => {
   store.dispatch('auth/initAuthentication')
     .then(user => {
       if (to.matched.some(route => route.meta.requiresAuth)) {
-        // protected route
         if (user) {
           next()
         } else {
           next({name: 'SignIn', query: {redirectTo: to.path}})
         }
       } else if (to.matched.some(route => route.meta.requiresGuest)) {
-        // protected route
         if (!user) {
           next()
         } else {
