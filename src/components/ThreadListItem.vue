7a4fe8544e87b5d44cf5495c6ef736852278e207
Airscript <francesco@airscript.it>
2023-01-19 20:51:58 +0100

    refactor: clean up ThreadListItem component in components folder

--- src/components/ThreadListItem.vue
+++ src/components/ThreadListItem.vue
@@ -6,6 +6,7 @@
           {{thread.title}}
         </router-link>
       </p>
+
       <p class="text-faded text-xsmall">
         By <a href="#">{{user.name}}</a>, <AppDate :timestamp="thread.publishedAt"/>.
       </p>
@@ -15,37 +16,31 @@
       <p class="replies-count">
         {{repliesCount}} replies
       </p>
-
-      <!--<img class="avatar-medium" src="http://i0.kym-cdn.com/photos/images/facebook/000/010/934/46623-batman_pikachu_super.png" alt="">-->
-
-      <!--<div>-->
-        <!--<p class="text-xsmall">-->
-          <!--<a href="#">Bruce Wayne</a>-->
-        <!--</p>-->
-        <!--<p class="text-xsmall text-faded">2 hours ago</p>-->
-      <!--</div>-->
     </div>
   </div>
 </template>
 
 <script>
-  import sourceData from '@/data'
+import sourceData from '@/data'
 
-  export default {
-    props: {
-      thread: {
-        required: true,
-        type: Object
-      }
+export default {
+  props: {
+    thread: {
+      type: Object,
+      required: true,
+    }
+  },
+
+  computed: {
+    repliesCount () {
+      return this.$store.getters['threads/threadRepliesCount'](
+        this.thread['.key']
+      )
     },
 
-    computed: {
-      repliesCount () {
-        return this.$store.getters['threads/threadRepliesCount'](this.thread['.key'])
-      },
-      user () {
-        return this.$store.state.users.items[this.thread.userId]
-      }
+    user () {
+      return this.$store.state.users.items[this.thread.userId]
     }
   }
+}
 </script>
