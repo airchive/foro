8531254230f7ccd4103a1082e86c5be78c0ae5c7
Airscript <francesco@airscript.it>
2023-01-19 02:03:45 +0100

    style: indent build.js

--- build/build.js
+++ build/build.js
@@ -11,20 +11,22 @@ const webpack = require('webpack')
 const config = require('../config')
 const webpackConfig = require('./webpack.prod.conf')
 
-const spinner = ora('building for production...')
+const spinner = ora('Building for production...')
 spinner.start()
 
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
   if (err) throw err
+
   webpack(webpackConfig, function (err, stats) {
     spinner.stop()
     if (err) throw err
+
     process.stdout.write(stats.toString({
       colors: true,
+      chunks: false,
       modules: false,
       children: false,
-      chunks: false,
-      chunkModules: false
+      chunkModules: false,
     }) + '\n\n')
 
     if (stats.hasErrors()) {
@@ -33,6 +35,7 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
     }
 
     console.log(chalk.cyan('  Build complete.\n'))
+
     console.log(chalk.yellow(
       '  Tip: built files are meant to be served over an HTTP server.\n' +
       '  Opening index.html over file:// won\'t work.\n'
