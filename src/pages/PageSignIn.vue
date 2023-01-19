72ff9f79d6f008f236a6b6ffb144041b28029b4c
Airscript <francesco@airscript.it>
2023-01-19 20:22:43 +0100

    refactor: clean up PageSignIn component in pages folder

--- src/pages/PageSignIn.vue
+++ src/pages/PageSignIn.vue
@@ -7,24 +7,53 @@
 
         <div class="form-group">
           <label for="email">Email</label>
+
           <input
             v-model="form.email"
             @blur="$v.form.email.$touch()"
-            id="email" type="text" class="form-input">
+            id="email" type="text" class="form-input"
+          >
+
           <template v-if="$v.form.email.$error">
-            <span v-if="!$v.form.email.required" class="form-error">This field is required</span>
-            <span v-else-if="!$v.form.email.email" class="form-error">This in not a valid email address</span>
+            <span
+              v-if="!$v.form.email.required"
+              class="form-error"
+            >
+              This field is required
+            </span>
+
+            <span
+              v-else-if="!$v.form.email.email"
+              class="form-error"
+            >
+              This in not a valid email address
+            </span>
           </template>
         </div>
+
         <div class="form-group">
           <label for="password">Password</label>
+
           <input
             v-model="form.password"
             @blur="$v.form.password.$touch()"
-            id="password" type="password" class="form-input">
+            id="password" type="password" class="form-input"
+          >
+
           <template v-if="$v.form.password.$error">
-            <span v-if="!$v.form.password.required" class="form-error">This field is required</span>
-            <span v-if="!$v.form.password.minLength" class="form-error">The password must be at least 6 characters long</span>
+            <span
+              v-if="!$v.form.password.required"
+              class="form-error"
+            >
+              This field is required
+            </span>
+
+            <span
+              v-if="!$v.form.password.minLength"
+              class="form-error"
+            >
+              The password must be at least 6 characters long
+            </span>
           </template>
         </div>
 
@@ -33,12 +62,20 @@
         </div>
 
         <div class="form-actions text-right">
-          <router-link :to="{name: 'Register'}">Create an account?</router-link>
+          <router-link :to="{name: 'Register'}">
+            Create an account?
+          </router-link>
         </div>
       </form>
 
       <div class="push-top text-center">
-        <button @click="signInWithGoogle" class="btn-red btn-xsmall"><i class="fa fa-google fa-btn"></i>Sign in with Google</button>
+        <button
+          @click="signInWithGoogle"
+          class="btn-red btn-xsmall"
+        >
+          <i class="fa fa-google fa-btn"></i>
+          Sign in with Google
+        </button>
       </div>
     </div>
   </div>
@@ -46,12 +83,13 @@
 
 <script>
     import { required, email, minLength } from 'vuelidate/lib/validators'
+
     export default {
       data () {
         return {
           form: {
             email: null,
-            password: null
+            password: null,
           }
         }
       },
@@ -59,12 +97,13 @@
       validations: {
         form: {
           email: {
+            email,
             required,
-            email
           },
+
           password: {
             required,
-            minLength: minLength(6)
+            minLength: minLength(6),
           }
         }
       },
@@ -72,6 +111,7 @@
       methods: {
         signIn () {
           this.$v.form.$touch()
+
           if (!this.$v.form.$invalid) {
             this.$store.dispatch('auth/signInWithEmailAndPassword', {
               email: this.form.email,
@@ -81,6 +121,7 @@
               .catch(error => alert('🤷‍️' + error.message))
           }
         },
+
         signInWithGoogle () {
           this.$store.dispatch('auth/signInWithGoogle')
             .then(() => this.successRedirect())
@@ -93,7 +134,3 @@
       }
     }
 </script>
-
-<style scoped>
-
-</style>
