7369f270afd621002b86b179a7d2604fe2009cd2
Airscript <francesco@airscript.it>
2023-01-19 20:51:24 +0100

    refactor: clean up TheNavbar component in components folder

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,5 +1,10 @@
 <template>
-  <header class="header" id="header" v-click-outside="closeMobileNavbar" v-handle-scroll="closeMobileNavbar">
+  <header
+    class="header"
+    id="header"
+    v-click-outside="closeMobileNavbar"
+    v-handle-scroll="closeMobileNavbar"
+  >
 
     <router-link
       :to="{name: 'Home'}"
@@ -9,32 +14,36 @@
     </router-link>
 
     <div class="btn-hamburger">
-      <!-- use .btn-humburger-active to open the menu -->
       <div class="top bar"></div>
       <div class="middle bar"></div>
       <div class="bottom bar"></div>
     </div>
 
-    <!-- use .navbar-open to open nav -->
     <nav class="navbar">
       <ul v-if="user">
         <li class="navbar-user">
           <a @click.prevent="userDropdownOpen = !userDropdownOpen">
             <img class="avatar-small" :src="user.avatar" alt="">
+
             <span>
                 {{user.name}}
-                <img class="icon-profile" src="../assets/img/arrow-profile.svg" alt="">
+
+                <img
+                  class="icon-profile"
+                  src="../assets/img/arrow-profile.svg"
+                  alt=""
+                >
             </span>
           </a>
 
-          <!-- dropdown menu -->
-          <!-- add class "active-drop" to show the dropdown -->
           <div id="user-dropdown" :class="{'active-drop': userDropdownOpen}">
             <div class="triangle-drop"></div>
+
             <ul class="dropdown-menu">
               <li class="dropdown-menu-item">
                 <router-link :to="{name: 'Profile'}">View Profile</router-link>
               </li>
+
               <li class="dropdown-menu-item">
                 <a @click.prevent="$store.dispatch('auth/signOut')">Sign Out</a>
               </li>
@@ -42,10 +51,12 @@
           </div>
         </li>
       </ul>
+
       <ul v-else>
         <li class="navbar-item">
           <router-link :to="{name: 'SignIn'}">Sign In</router-link>
         </li>
+
         <li class="navbar-item">
           <router-link :to="{name: 'Register'}">Register</router-link>
         </li>
@@ -55,29 +66,26 @@
 </template>
 
 <script>
-  import {mapGetters} from 'vuex'
-  import clickOutside from '@/directives/click-outside'
-  import handleScroll from '@/directives/handle-scroll'
-  export default {
-    directives: {
-      clickOutside,
-      handleScroll
-    },
+import {mapGetters} from 'vuex'
+import clickOutside from '@/directives/click-outside'
+import handleScroll from '@/directives/handle-scroll'
 
-    data () {
-      return {
-        userDropdownOpen: false
-      }
-    },
+export default {
+  directives: {
+    clickOutside,
+    handleScroll,
+  },
 
-    computed: {
-      ...mapGetters({
-        'user': 'auth/authUser'
-      })
+  data () {
+    return {
+      userDropdownOpen: false
     }
+  },
+
+  computed: {
+    ...mapGetters({
+      'user': 'auth/authUser'
+    })
   }
+}
 </script>
-
-<style scoped>
-
-</style>
